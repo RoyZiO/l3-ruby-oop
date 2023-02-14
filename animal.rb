5056e218decfde5f4e46b9075097f82b3f195091
RoyZiO <nziokinroy@gmail.com>
2023-02-14 12:09:33 +0300

    Completed wk2 day2 codealong

--- animal.rb
+++ animal.rb
@@ -1,12 +1,46 @@
 # TODO: CREATE CLASS AND INSTANCE METHODS (INCLUDE VARIABLES)
 class Animal        #all classes in Ruby should be in a combiation of SentenceCase and CamelCase
     
-    def initialize(name)
+    attr_reader :limbs
+    attr_writer :limbs
+
+    attr_accessor :height, :name, :lifespan
+
+    @lifespan = 24
+    
+
+    def initialize(name, lifespan)
+        @name = name 
+        @lifespan = lifespan
         puts "#{name} has been created"
+        @species = "Homo Sapiens Sapiens"
+        @limbs = 0
     end
+
+    #getter method for species 
+    #def species_name
+   #     @species
+   # end
+
+    #setter method for species
+   # def species_name=(value)
+   #     @species =  value
+   # end
+
+    # getter method for lifespan
+    #def species_lifespan
+       # @lifespan
+   # end
+
+    # setter method for lifespan
+   # def species_lifespan=(value)
+    #   @lifespan = value 
+    #end
+
 
     #play sound
     def play_sound
+        @sound = "but"
         puts "Playing Sound"
     end
 
@@ -19,8 +53,36 @@ class Animal        #all classes in Ruby should be in a combiation of SentenceCa
 end
 
 #object or #instances
-zebra = Animal.new("African Zebra")
-giraffe = Animal.new("African Giraffe")
+zebra = Animal.new("African Zebra", 25)
+giraffe = Animal.new("African Giraffe", 25)
+
+#giraffe.species_name = "Giraffa"
+
+#puts giraffe.species_name
+
+#zebra.species_name = "Equuis quagga"
+#puts zebra.species_name
+
+
+#giraffe.species_lifespan = 20
+#puts giraffe.species_lifespan
+
+#zebra.species_lifespan = 35
+#puts zebra.species_lifespan
+
+giraffe.limbs = 4
+puts giraffe.limbs
+
+zebra.limbs = 4
+puts zebra.limbs
+
+
+giraffe.height = 5.2
+puts giraffe.height
+
+zebra.height = 4.1
+puts zebra.height
+
 
 puts zebra
 puts giraffe
